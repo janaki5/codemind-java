a2f502c6e36bfdd52cb7f50fee505ada6983d601
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-04 11:25:24 +0530

    import java.util.Scanner; class Jaava {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int h,s,sp;         h=sc.nextInt();         s=sc.nextInt();         sp=sc.nextInt();         if(h>50 && s>60 && sp>100)           System.out.println("10");         else if(h>50 && s>60 )           System.out.println("9");         else if(s>60 && sp>100 )           System.out.println("8");         else if(h>50  && sp>100)           System.out.println("7");
            else if(h>50 || s>60 || sp>100)
              System.out.println("6");
            else
              System.out.println("5");
              
        }
    }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Jaava
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int h,s,sp;
+        h=sc.nextInt();
+        s=sc.nextInt();
+        sp=sc.nextInt();
+        if(h>50 && s>60 && sp>100)
+          System.out.println("10");
+        else if(h>50 && s>60 )
+          System.out.println("9");
+        else if(s>60 && sp>100 )
+          System.out.println("8");
+        else if(h>50  && sp>100)
+          System.out.println("7");
+        
+        else if(h>50 || s>60 || sp>100)
+          System.out.println("6");
+        else
+          System.out.println("5");
+          
+    }
+}
