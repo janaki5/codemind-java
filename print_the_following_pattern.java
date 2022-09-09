e0f6fbe28c7d22d8ae0d284cd91519258a93bf8d
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-09 13:54:56 +0530

    import java.util.Scanner; class Pattern5 {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 System.out.print((char)(64+i)+" ");             }             System.out.println( );         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -8,9 +8,9 @@ class Pattern5
         n=sc.nextInt();
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-i+1;j++)
+            for(int j=1;j<=n;j++)
             {
-                System.out.print(j);
+                System.out.print((char)(64+i)+" ");
             }
             System.out.println( );
         }
