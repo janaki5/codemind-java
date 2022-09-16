faa3cf46e4919a3cb3af9b089b298fac37d79bff
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-16 22:01:01 +0530

    import java.util.Scanner; class Code {     public static void main(String args[])     {         int a,c=0,z;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {          b[i]=sc.nextInt();         }         z=sc.nextInt();        for(int i=0;i<a;i++)         {             if(b[i]==z)             {             c++;             }
            }
            if(c==0)
            System.out.println("False");
            else
            System.out.println("True");
            
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Code
+{
+    public static void main(String args[])
+    {
+        int a,c=0,z;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+         b[i]=sc.nextInt();
+        }
+        z=sc.nextInt();
+       for(int i=0;i<a;i++)
+        {
+            if(b[i]==z)
+            {
+            c++;
+            }
+        
+        }
+        if(c==0)
+        System.out.println("False");
+        else
+        System.out.println("True");
+        
+    }
+}
