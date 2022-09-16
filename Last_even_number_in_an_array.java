3c2272c814bd6c552dd9abfc2ea115aa37f4d9b9
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-16 21:47:08 +0530

    import java.util.Scanner; class Code {     public static void main(String args[])     {         int a,c=0;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {          b[i]=sc.nextInt();         }        for(int i=0;i<a;i++)         {             if(b[i]%2==0)             {                 c=b[i];             }         }         System.out.println(c);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Code
+{
+    public static void main(String args[])
+    {
+        int a,c=0;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+         b[i]=sc.nextInt();
+        }
+       for(int i=0;i<a;i++)
+        {
+            if(b[i]%2==0)
+            {
+                c=b[i];
+            }
+        }
+        System.out.println(c);
+    }
+}
