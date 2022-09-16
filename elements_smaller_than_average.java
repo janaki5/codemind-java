4ab99f6b35142f7ab46292e7f1253ebb4fb921be
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-16 21:21:49 +0530

    import java.util.Scanner; class Code {     public static void main(String args[])     {         int a,sum=0,c=0,co=0;         float avg=0;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {          b[i]=sc.nextInt();         }         for(int i=0;i<a;i++)         {             sum=sum+b[i];         }         c=sum/a;
            for(int j=0;j<a;j++)
            {
                if(b[j]<=c)
                {
                    co++;
                }
            }
            System.out.print(co+" ");
        }
    }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Code
+{
+    public static void main(String args[])
+    {
+        int a,sum=0,c=0,co=0;
+        float avg=0;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+         b[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++)
+        {
+            sum=sum+b[i];
+        }
+        c=sum/a;
+        
+        for(int j=0;j<a;j++)
+        {
+            if(b[j]<=c)
+            {
+                co++;
+            }
+        }
+        System.out.print(co+" ");
+    }
+}
