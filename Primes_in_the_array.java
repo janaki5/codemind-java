ae5516c1fd962896276ca60493a8466e6fcc005c
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-17 14:19:39 +0530

    import java.util.Scanner; class Array {     public static boolean isprime(int n)     {       int count=0;       for(int i=2;i<=(int)Math.sqrt(n);i++)       {           if(n%i==0)             count++;       }       if(count==0)         return true;       else         return false;     }
      public static void main (String args[])
      {
       Scanner sc=new Scanner (System.in);
       int x[],i,n,co=0;
       n=sc.nextInt();
       
       x=new int[n];
       for(i=0;i<n;i++)
       {
         x[i]=sc.nextInt();
       }
       
    for(i=0;i<n;i++)
    {
     if(x[i]==1)
     continue;
     if (isprime(x[i]))
     co++;
    }
    System.out.println(co);
    }
    }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Array
+{
+    public static boolean isprime(int n)
+    {
+      int count=0;
+      for(int i=2;i<=(int)Math.sqrt(n);i++)
+      {
+          if(n%i==0)
+            count++;
+      }
+      if(count==0)
+        return true;
+      else
+        return false;
+    }
+   
+  public static void main (String args[])
+  {
+   Scanner sc=new Scanner (System.in);
+   int x[],i,n,co=0;
+   n=sc.nextInt();
+   
+   x=new int[n];
+   for(i=0;i<n;i++)
+   {
+     x[i]=sc.nextInt();
+   }
+   
+for(i=0;i<n;i++)
+{
+ if(x[i]==1)
+ continue;
+ if (isprime(x[i]))
+ co++;
+}
+System.out.println(co);
+}
+}
+
