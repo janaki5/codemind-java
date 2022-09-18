bb3dcddf86607904784436765390cb39eeb51ad6
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-18 12:15:53 +0530

    import java.util.Scanner; class HappyNumber {     public static int isHappyNumber(int num){         int rem = 0, sum = 0;
            while(num > 0){  
                rem = num%10;  
                sum = sum + (rem*rem);  
                num = num/10;  
            }  
            return sum;  
        }  
          
        public static void main(String[] args) {  
            int num ;
            Scanner sc=new Scanner(System.in);
            num=sc.nextInt();
            int result = num;  
              
            while(result != 1 && result != 4){  
                result = isHappyNumber(result);  
            }  
              
             
            if(result == 1)  
                System.out.println( "True");  
            else if(result == 4)  
                System.out.println("False");     
        }  
    }

---
+++ happy_number.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class HappyNumber  
+{     
+    public static int isHappyNumber(int num){  
+        int rem = 0, sum = 0;  
+          
+        
+        while(num > 0){  
+            rem = num%10;  
+            sum = sum + (rem*rem);  
+            num = num/10;  
+        }  
+        return sum;  
+    }  
+      
+    public static void main(String[] args) {  
+        int num ;
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+        int result = num;  
+          
+        while(result != 1 && result != 4){  
+            result = isHappyNumber(result);  
+        }  
+          
+         
+        if(result == 1)  
+            System.out.println( "True");  
+        else if(result == 4)  
+            System.out.println("False");     
+    }  
+}  
