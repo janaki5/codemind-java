c1c9269c7dd1c363775063b665ef8172401684b9
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-22 22:25:11 +0530

    import java.util.Scanner; class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r=0,d=0,temp,t;         n=sc.nextInt();         t=n*n;         temp=n;         while(t!=0)         {             r=t%10;             d=d+r;             t=t/10;         }         if(d==temp)         System.out.println("Neon Number");         else         System.out.println("Not Neon Number");     } }

--- Neon_Number.java
+++ Neon_Number.java
@@ -4,20 +4,18 @@ class Code
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int m,b,n,sq1,rem=0,sum=0;
-        
+        int n,r=0,d=0,temp,t;
         n=sc.nextInt();
-        b=n*n;
-        while(b>0)
+        t=n*n;
+        temp=n;
+        while(t!=0)
         {
-            rem=b%10;
-            sum=sum+rem;
-            b=b/10;
+            r=t%10;
+            d=d+r;
+            t=t/10;
         }
-        if(sum==n)
-        {
-         System.out.println("Neon Number");
-        }
+        if(d==temp)
+        System.out.println("Neon Number");
         else
         System.out.println("Not Neon Number");
     }
