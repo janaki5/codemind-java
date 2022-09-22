8902be2468b9cb219018767b91f816cd2cc071eb
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-22 14:38:43 +0530

    import java.util.Scanner; class Coswe {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,count=0;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             if(i*(i+1)==n)             {             count=1;
                }
                
            }
            if(count==1)
      System.out.println("YES");
      else
                System.out.println("NO");
        }
    }

--- Pronic_Number_or_Heteromecic_Number.java
+++ Pronic_Number_or_Heteromecic_Number.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
-class Prime
+class Coswe
 {
     public static void main(String args[])
     {
-     Scanner sc=new Scanner(System.in);
-     int n,p=0;
-     n=sc.nextInt();
-     for(int i=1;i<=n;i++)
-     {
-         if(i*(i+1)==n)
-         {
-          p=1;
-          break;
-         }
-     }
-        if(p==1)
-          System.out.println("YES");
-         else
-          System.out.println("NO");
-         
-     }
+        Scanner sc=new Scanner(System.in);
+        int n,count=0;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            if(i*(i+1)==n)
+            {
+            count=1;
+                
+            }
+            
+        }
+        if(count==1)
+  System.out.println("YES");
+  else
+            System.out.println("NO");
     }
+}
