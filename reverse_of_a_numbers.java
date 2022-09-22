db06bf19a9ee0ef6af23a7e9d3686639de05b982
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-09-22 23:01:54 +0530

    import java.util.Scanner; class code {     public static void main(String args[])     {     Scanner sc=new Scanner(System.in);     int n,r=0,d=0;     n=sc.nextInt();     while(n!=0)     {         r=n%10;         d=d*10+r;         n=n/10;
        }
        System.out.println(d);
        }
    }

---
+++ reverse_of_a_numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class code
+{ 
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int n,r=0,d=0;
+    n=sc.nextInt();
+    while(n!=0)
+    {
+        r=n%10;
+        d=d*10+r;
+        n=n/10;
+        
+    }
+    System.out.println(d);
+    }
+}
