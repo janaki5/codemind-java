a2bd85d5e330e7ec63a1aea7f91bff6933347501
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-10-03 19:45:37 +0530

    import java.util.Scanner; class PrimesInRange {   public static boolean isPrime(int n)   {      int count=0; 	 for(int i=2;i<=(int)Math.sqrt(n);i++) 	 { 	    if(n%i==0) 		  count++; 	 } 	 if(count==0) 	    return true; 	 else 	    return false;   }   public static boolean isPal(int n)   {       int r=0,sum=0,temp=0;       temp=n;       while(n!=0)       {        r=n%10;        sum=sum*10+r;        n=n/10;       }       if(sum==temp)          return true;        else           return false;   }    public static void main(String args[])   {      Scanner sc=new Scanner(System.in); 	 int n,c=0; 	 n=sc.nextInt(); 	 for(int i=n+1;;i++) 	 { 	     if(isPrime(i)&&isPal(i)) 		 { 				c=i; 				break;
    		 }
    	 }
    	  System.out.println(c);
      }
      
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,46 +1,49 @@
 import java.util.Scanner;
-class Prime
+class PrimesInRange
 {
-    public static boolean isprime(int n)
-    {
-      int count=0;
-      for(int i=2;i<=(int)Math.sqrt(n);i++)
+  public static boolean isPrime(int n)
+  {
+     int count=0;
+	 for(int i=2;i<=(int)Math.sqrt(n);i++)
+	 {
+	    if(n%i==0)
+		  count++;
+	 }
+	 if(count==0)
+	    return true;
+	 else
+	    return false;
+  }
+  public static boolean isPal(int n)
+  {
+      int r=0,sum=0,temp=0;
+      temp=n;
+      while(n!=0)
       {
-          if(n%i==0)
-            count++;
+       r=n%10;
+       sum=sum*10+r;
+       n=n/10;
       }
-      if(count==0)
-        return true;
-      else
-        return false;
-    }
-    public static boolean isPal(int n)
-    {
-        int r,sum=0,temp;
-        temp=n;
-        while(n>0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        if(sum==temp)
+      if(sum==temp)
          return true;
-        else 
-         return false;
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        for(int i=n+1; ;i++)
-        {
-            if(isprime(i) && isPal(i))
-            {
-              System.out.println(i);
-             break;
-            }
-        }
-    }
+       else
+          return false;
+  }
+   public static void main(String args[])
+  {
+     Scanner sc=new Scanner(System.in);
+	 int n,c=0;
+	 n=sc.nextInt();
+	 for(int i=n+1;;i++)
+	 {
+	     if(isPrime(i)&&isPal(i))
+		 {
+				c=i;
+				break;
+		   
+		 }
+	 }
+	  System.out.println(c);
+  }
+  
 }
